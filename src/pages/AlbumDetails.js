b8bc61998e4e3380647164105f61dc4cfab78eed
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2022-03-03 20:56:01 +0000

    Added svg icons to album details page

--- src/pages/AlbumDetails.js
+++ src/pages/AlbumDetails.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react';
 import dayjs from 'dayjs';
 import sanityClient from '../sanity.js';
 import { Link, useParams } from 'react-router-dom';
+import amazonIcon from '../img/amazon.svg';
+import appleIcon from '../img/apple.svg';
+import bandcampIcon from '../img/bandcamp.svg';
+import itunesIcon from '../img/itunes.svg';
+import spotifyIcon from '../img/spotify.svg';
 
 export default function AlbumDetails() {
   const [albumDetails, setAlbumDetails] = useState(null);
@@ -36,6 +41,11 @@ export default function AlbumDetails() {
                 <Link to={'/artists'}>ARTISTS</Link> / <Link key={albumDetails.albumArtist.slug.current} to={'/artists/' + albumDetails.albumArtist.slug.current}>{albumDetails.albumArtist.name}</Link>
                 <h1 className='album-details-album-title'>{albumDetails.albumTitle}</h1>
                 <h4 className='album-details-album-date'>Released on {dayjs(albumDetails.releaseDate).format('D MMMM YYYY')}</h4>
+                <img className='album-details-icon' src={amazonIcon} alt="" />
+                <img className='album-details-icon' src={appleIcon} alt="" />
+                <img className='album-details-icon' src={bandcampIcon} alt="" />
+                <img className='album-details-icon' src={itunesIcon} alt="" />
+                <img className='album-details-icon' src={spotifyIcon} alt="" />
               </div>
             </div>
           </div>;
