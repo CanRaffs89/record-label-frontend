ecf017e038623122d0306a94f2c07a74c48cde8f
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2022-05-24 20:53:49 +0100

    Finished Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
 export default function Contact() {
-  return <div className="page-container">
-      <div className="map-container">
-        <img src="https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s+555555(-0.07155,51.52403)/-0.0717,51.5241,14.41,0/500x500?access_token=pk.eyJ1IjoiY2FucmFmZnM4OSIsImEiOiJjbDNhbGVrb2EwMnAzM2pwNGxjb3Y2bTAxIn0.ZfsucNrF8679m-Z8whKP9A" alt="Map location" />
+  const mapImage = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s+555555(-0.07155,51.52403)/-0.0717,51.5241,14.41,0/500x500?access_token=pk.eyJ1IjoiY2FucmFmZnM4OSIsImEiOiJjbDNhbGVrb2EwMnAzM2pwNGxjb3Y2bTAxIn0.ZfsucNrF8679m-Z8whKP9A";
+
+  return <div className="page-container flex-page-container">
+      <div className="contact-map-container">
+        <img src={mapImage} alt="Map showing location of the shop at the junction of Brick Lane and Bacon Street in London" />
+      </div>
+      <div className="contact-text-container">
+        <p>We are a small independent record label and shop located in the heart of London at the junction of Brick Lane and Bacon Street. Pop in and see us for new releases, merchandise, in-store events and more!</p>
+        <p class="contact-text-heading">Opening Times</p>
+        <p>Monday - <span class="contact-text-time">9:00 - 5:00</span></p>
+        <p>Tuesday - <span class="contact-text-time">9:00 - 5:00</span></p>
+        <p>Wednesday - <span class="contact-text-time">9:00 - 3:00</span></p>
+        <p>Thursday - <span class="contact-text-time">9:00 - 8:00</span></p>
+        <p>Friday - <span class="contact-text-time">9:00 - 5:00</span></p>
+        <p>Saturday - <span class="contact-text-time">10:00 - 4:00</span></p>
+        <p>Sunday - <span class="contact-text-time">Closed</span></p>
       </div>
   </div>
 }
